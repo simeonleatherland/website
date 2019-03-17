c7e7fabf75b33b276907b27fa8e46ea182556706
Simeon Leatherland <simeon.leatherland@gmail.com>
2019-03-17 14:38:37 +0800

    trying something

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,7 +19,21 @@ $(document).ready(function() {
 
 });
 
+function goToByScroll(id){
+    // Reove "link" from the ID
+id = id.replace("link", "");
+    // Scroll
+$('html,body').animate({
+    scrollTop: $("#"+id).offset().top},
+    'slow');
+}
 
+$("h4 > a").click(function(e) { 
+    // Prevent a page reload when a link is pressed
+e.preventDefault(); 
+    // Call the scroll function
+goToByScroll($(this).attr("id"));           
+});
 
 function setup_progress_bar_animation()
 {
